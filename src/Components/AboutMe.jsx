86be70ac7835ab6951fcca74c410e863b511d4cf
bayzedahmedandthe <bayzedm784@.com>
2025-05-14 08:01:51 +0600

    Finish

--- src/Components/AboutMe.jsx
+++ src/Components/AboutMe.jsx
@@ -6,22 +6,24 @@ import Aos from 'aos';
 const AboutMe = () => {
     const options = {
         animationData: animationData,
-        loop: true,       
-        autoplay: true,     
+        loop: true,
+        autoplay: true,
     };
     useEffect(() => {
         Aos.init();
     }, [])
     return (
-        <div  id="aboutMe" className="lg:w-10/12 max-w-[90%]  mx-auto my-12">
+        <div id="aboutMe" className="lg:w-10/12 max-w-[90%]  mx-auto my-12">
             <div className="md:flex items-center justify-between gap-20 border-b-[#39FF14] border-b ">
                 <div data-aos="fade-down" data-aos-duration="1000" className="flex justify-center items-center">
                     <Lottie options={options} height={320} width={320} />
                 </div>
                 <div data-aos="fade-up" data-aos-duration="1000" className="md:w-[70%]">
-                    <h2 className="md:text-3xl text-center text-xl font-semibold pt-8 pb-4  text-white">About me</h2>
+                    <h2 className="md:text-3xl text-xl font-semibold pt-8 pb-4  text-white">About me</h2>
                     <p className="text-white">
-                        Hello! I am a passionate  frontend developer who loves creating beautiful and interactive web experiences. My coding journey started with a curiosity for web design, which gradually turned into a deep passion. I enjoy working with HTML, CSS, JavaScript, React, Firebase, and other modern web technologies. My goal is to build high-performance, responsive web applications. Outside of coding, I enjoy to travel. Learning new technologies and creating innovative solutions is what drives me!
+                        Hello! I am a MERN Stack Developer (MongoDB, Express.js, React.js, Node.js) focused on building high-performance and user-friendly web applications. I believe that the proper use of modern technologies can help any company move forward digitally.
+                        As a developer, I don't just build features—I deliver solutions that enhance productivity, improve user experience, and support business growth. I am skilled in responsive design, writing clean code, and collaborating effectively within a team.
+                        Learning new technologies, solving problems, and working on innovative projects are what drive me. My goal is to develop web applications that align with a company’s values while meeting users' needs.
                     </p>
                 </div>
 
--- src/Components/Projects.jsx
+++ src/Components/Projects.jsx
@@ -1,6 +1,5 @@
 import resturant from "../assets/bistro_boss.webp"
-import job from "../assets/2895265.jpg"
-import review from "../assets/5384700.jpg"
+import reviews from "../assets/reviews.png"
 import react_logo from "../assets/react.png"
 import firebase_logo from "../assets/1_R4c8lHBHuH5qyqOtZb3h-w-removebg-preview.png"
 import tailwind_logo from "../assets/download-removebg-preview.png"
@@ -17,14 +16,16 @@ const Projects = () => {
         <div id="projects" className=" lg:w-10/12 w-[90%] mx-auto my-8">
             <h2 className="md:text-2xl font-semibold text-lg py-8">Some of the projects I have done</h2>
             {/* Bistro Boss */}
-            <div className="flex gap-8">
+            <div className="flex gap-8 pb-8">
                 {/* Img Container */}
-                <div className="flex items-center justify-center p-2">
-                    <img className="md:h-[300px] md:w-[460px] rounded-sm" src={resturant} alt="" />
+                <div data-aos="fade-down" data-aos-duration="1000"
+                 className="flex items-center justify-center p-2">
+                    <img className="md:h-[285px] md:w-[460px] rounded-sm transition-transform duration-400 transform hover:scale-110" src={resturant} alt="" />
                 </div>
                 {/* Text Container */}
-                <div className="flex-1">
-                    <h3 className="text-xl text-gray-100 pt-2">Bistro Boss Restaurant</h3>
+                <div data-aos="fade-up" data-aos-duration="1000"
+                className="flex-1">
+                    <h3 className="text-xl text-gray-100 ">Bistro Boss Restaurant</h3>
                     <p className="text-gray-300 py-3">This web application allows users to order food easily and securely, while providing admins with an efficient order management dashboard.
                     </p>
                     <p className="text-gray-300 ">
@@ -51,21 +52,21 @@ const Projects = () => {
                         </div>
 
                         <div className="button-wrapper hover:shadow-md hover:shadow-green-500">
-                            <button className='animated-border-button px-4 py-1'><a target="_blank" href="https://github.com/bayzedahmedandthe/Dev_Discuss_Server"> Backend code </a></button>
+                            <button className='animated-border-button px-4 py-1'><a target="_blank" href="https://github.com/bayzedahmedandthe/bistro-boss-server"> Backend code </a></button>
                         </div>
                     </div>
                 </div>
             </div>
             {/* Service review */}
-            <div className="flex gap-8 my-16">
+            <div className="flex gap-8 py-8 border-t border-green-400">
                 {/* Text Container */}
-                <div className="flex-1">
+                <div data-aos="fade-up" data-aos-duration="1000"
+                 className="flex-1">
                     <h3 className="text-xl text-gray-100 pt-2">Service Review</h3>
-                    <p className="text-gray-300 py-3">This web application allows users to order food easily and securely, while providing admins with an efficient order management dashboard.
+                    <p className="text-gray-300 py-3">Service Review is a modern and user-friendly website where users can share opinions and reviews about various services. It facilitates transparent and informed decision-making
                     </p>
                     <p className="text-gray-300 ">
-                        Responsive UI ,Role-based Dashboards, Secure Payments, Secure Login, Form Validation, Admin
-                        moderation Tools, Privet routes, JWT authentication
+                        Responsive UI, Add service, Delete service, Add reviews, Service card, Search service, Privet routes
                     </p>
                     <h5 className="py-2">Used Technologies</h5>
                     {/* Logo container */}
@@ -80,20 +81,21 @@ const Projects = () => {
                     <div className="flex items-center gap-6 pt-4">
 
                         <div className="button-wrapper hover:shadow-md hover:shadow-green-500">
-                            <button className='animated-border-button px-4 py-1'><a target="_blank" href="https://bistro-boss-3bc7d.web.app/"> Live Demo </a></button>
+                            <button className='animated-border-button px-4 py-1'><a target="_blank" href="https://assaignment-11-5c7ac.web.app/"> Live Demo </a></button>
                         </div>
                         <div className="button-wrapper hover:shadow-md hover:shadow-green-500">
-                            <button className='animated-border-button px-4 py-1'><a target="_blank" href="https://github.com/bayzedahmedandthe/bistro-boss-client-site"> Client code</a></button>
+                            <button className='animated-border-button px-4 py-1'><a target="_blank" href="https://github.com/bayzedahmedandthe/Service-Review"> Client code</a></button>
                         </div>
 
                         <div className="button-wrapper hover:shadow-md hover:shadow-green-500">
-                            <button className='animated-border-button px-4 py-1'><a target="_blank" href="https://github.com/bayzedahmedandthe/Dev_Discuss_Server"> Backend code </a></button>
+                            <button className='animated-border-button px-4 py-1'><a target="_blank" href="https://github.com/bayzedahmedandthe/Service-Review-Server"> Backend code </a></button>
                         </div>
                     </div>
                 </div>
                 {/* Img Container */}
-                <div className="flex items-center justify-center p-2">
-                    <img className="md:h-[300px] md:w-[460px] rounded-sm" src={resturant} alt="" />
+                <div data-aos="fade-down" data-aos-duration="1000"
+                 className="flex items-center justify-center p-2">
+                    <img className="md:h-[300px] md:w-[460px] rounded-sm transition-transform duration-400 transform hover:scale-110" src={reviews} alt="" />
                 </div>
             </div>
         </div>
